6a18e08fc5d555776818b137313ee8bc1a2b8f5b
Patrick Lewis <plewis@binghamton.edu>
2012-10-10 14:11:44 -0400

    Remove unneeded comment block

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php  
 
-/**
- * Short description goes here
- *
- * PHP version 5
- *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
- *
- * @category Stale_Sites
- * @package  Stale_Sites
- * @author   Patrick Lewis <plewis@binghamton.edu>
- * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
- * @link     http://www2.binghamton.edu/
-**/
 include_once 'config.php';
 
 //unlink('cache.html')
