ca5234537b525c8ae5c2ed34b87b7deb9236506c
Adarsha <adarshakb@adarshakb-Lenovo-U410.(none)>
2012-11-14 12:14:03 -0500

    New feature to ignore specific file names
    New feature to ignore specific file names given in the configuration
    folder and thus able to represent more better the actual life-span of
    a project.
    
    Signed-off-by:  <adarshakb>

--- getStaleSites.php
+++ getStaleSites.php
@@ -4,7 +4,7 @@ include_once 'config.php';
 
 if($basePath !== null) {
 	try {
-		$ss = new SiteScanner($basePath, $ignoredSites);
+		$ss = new SiteScanner($basePath, $ignoredSites, $ignoredFileNames);
 		$ss->scanSites();
 		//$ss->displayReport($basePath); //Bug https://github.com/BinghamtonUniversity/stale-sites/issues/10
 		$ss->displayReport();
