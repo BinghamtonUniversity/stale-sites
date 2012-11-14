1fb35de8d2656fea08dffa9ce53d17d9797c1116
Adarsha <adarshakb@adarshakb-Lenovo-U410.(none)>
2012-11-14 11:53:39 -0500

    Bug fix: Issue 10
    Signed-off-by:  <adarshakb@gmail.com>

--- getStaleSites.php
+++ getStaleSites.php
@@ -4,9 +4,10 @@ include_once 'config.php';
 
 if($basePath !== null) {
 	try {
-	$ss = new SiteScanner($basePath, $ignoredSites);
-	$ss->scanSites();
-	$ss->displayReport($basePath);
+		$ss = new SiteScanner($basePath, $ignoredSites);
+		$ss->scanSites();
+		//$ss->displayReport($basePath); //Bug https://github.com/BinghamtonUniversity/stale-sites/issues/10
+		$ss->displayReport();
 	}
 	catch(Exception $e) {
 		echo $e->getMessage();
