7b85599f9cace2aa17b53cdb71f6d34c24029528
Adarsha <adarshakb@adarshakb-Lenovo-U410.(none)>
2012-12-10 15:55:43 -0500

    Commit problem fixed
    Signed-off-by:  <adarshakb@gmail.com>

---
+++ getStaleSites.php
@@ -0,0 +1,37 @@
+<?php
+include_once 'SiteScanner.class.php';
+include_once 'config.php';
+
+if($basePath !== null) {
+	try {
+		$ss = new SiteScanner($basePath, $ignoredSites, $ignoredFileNames);
+		$ss->scanSites();
+		//$ss->displayReport($basePath); //Bug https://github.com/BinghamtonUniversity/stale-sites/issues/10
+		$ans = $ss->getReport("http://".$_SERVER['HTTP_HOST']);
+
+		?>
+		<table class="table table-striped table-hover table-bordered">
+			<tr>
+				<th>X</th>
+				<th>Name</th>
+				<th>Days</th>
+			</tr>
+			<?php
+			foreach ($ans as $key => $value) {
+			?>
+			<tr>
+				<td><a class="icon-remove" href="addExcludeUrls.php?url=<?=urlencode($key)?>"></a></td>
+				<td><a href="/<?=$key?>"><?=$key?></a></td>
+				<td><?=$value?></td>
+			</tr>
+			<?php
+			}
+			?>
+		</table>
+	<?php
+	}
+	catch(Exception $e) {
+		echo $e->getMessage();
+	}
+}
+?>
